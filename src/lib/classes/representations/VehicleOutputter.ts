d9a9e480c6b1c3474c6f6fc6c241e8411ae6be5d
Melody Parker <melodyechoparker@gmail.com>
2024-10-09 13:10:03 -0500

    updated VehicleOutputter

--- src/lib/classes/representations/VehicleOutputter.ts
+++ src/lib/classes/representations/VehicleOutputter.ts
@@ -8,20 +8,28 @@ export default class VehicleOutputter {
   rate: number;
   directionOfOutput: boolean;
   startingVelocity: number;
+  minBetweenOutputs: number;
+  timeSinceLastOutput: number = Number.MAX_VALUE;
 
-  constructor(world: World, road:Road, posOnRoad: number, directionOnRoad: boolean, rate: number, startingVelocity: number) { 
+  constructor(world: World, road:Road, posOnRoad: number, directionOnRoad: boolean, rateOfOutput: number, startingVelocity: number, minBetweenOutputs: number = 0) { 
     // rate measured in average cars per second
     this.world = world;
     this.road = road;
     this.posOnRoad = posOnRoad;
-    this.rate = rate;
+    this.rate = rateOfOutput;
     this.directionOfOutput = directionOnRoad;
     this.startingVelocity = startingVelocity;
+    this.minBetweenOutputs = minBetweenOutputs;
   }
 
   update(dt: number): void {
     let probabilityOfGeneratingCar = dt * this.rate;
-    if (Math.random() > probabilityOfGeneratingCar)
+    if (Math.random() < probabilityOfGeneratingCar && this.timeSinceLastOutput >= this.minBetweenOutputs) {
       this.world.addVehicle(this.road, this.directionOfOutput, this.startingVelocity, this.posOnRoad);
+      this.timeSinceLastOutput = 0;
+    }
+    else {
+      this.timeSinceLastOutput += dt;
+    }
   }
 }
