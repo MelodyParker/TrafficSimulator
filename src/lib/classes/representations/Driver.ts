9c4a384588691a663af16f752155cb84e20fbe4d
MelodyParker <102301694+MelodyParker@users.noreply.github.com>
2024-10-24 14:46:08 -0500

    adding personalities

--- src/lib/classes/representations/Driver.ts
+++ src/lib/classes/representations/Driver.ts
@@ -1,6 +1,7 @@
 import World from "./World";
 import { highestIds } from "$lib/misc/stores";
 import type Vehicle from "./Vehicle";
+import type Personality from "./Personality";
 
 
 
@@ -8,10 +9,13 @@ export default class Driver {
   vehicle: Vehicle;
   desiredFollowingDistance: number = 4.5;
   maxAcceleration: number;
+  personality: Personality;
+
   
-  constructor(vehicle: Vehicle) {
+  constructor(vehicle: Vehicle, personality: Personality) {
     this.vehicle = vehicle;
     this.maxAcceleration = vehicle.maxAcceleration;
+    this.personality = personality;
     
   }
 
---
+++ src/lib/classes/representations/Personality.ts
@@ -0,0 +1,15 @@
+export default class Personality {
+  desiredFollowingDistance: number; // the distance (in world coordinates) between this driver's vehicle and the vehicle in front of it
+  desiredBrakingAcceleration: number; // the acceleration this driver would like to apply when braking
+  desiredStoppingDistance: number; // the distance behind the next vehicle that this driver would like to stop
+  desiredSpeedOverLimit: number; // how much faster than the speed limit the driver wants to go
+
+  constructor(desiredFollowingDistance: number, desiredBrakingAcceleration: number, desiredStoppingDistance: number, desiredSpeedOverLimit: number) {
+    this.desiredFollowingDistance = desiredFollowingDistance;
+    this.desiredBrakingAcceleration = desiredBrakingAcceleration;
+    this.desiredStoppingDistance = desiredStoppingDistance;
+    this.desiredSpeedOverLimit = desiredSpeedOverLimit;
+
+
+  }
+}
